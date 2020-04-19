6e3aa3ef3f2763d926373f2183a02f63722abd1f
Ben Toosey <41492238+btoosey@users.noreply.github.com>
2020-04-19 10:50:54 +0100

    FEAT: Working thirst bar

--- Ludum Dare 46/Assets/Scripts/Plant.cs
+++ Ludum Dare 46/Assets/Scripts/Plant.cs
@@ -6,15 +6,21 @@ public class Plant : MonoBehaviour
 {
 
     public int plantMaxHealth = 30;
+    public int plantMaxThirst = 30;
     public int currentHealth;
+    public int currentThirst;
 
     public HealthBar healthbar;
+    public ThirstBar thirstbar;
 
     void Start()
     {
         currentHealth = plantMaxHealth;
+        currentThirst = plantMaxThirst;
         healthbar.SetMaxHealth(plantMaxHealth);
+        thirstbar.SetMaxThirst(plantMaxThirst);
         InvokeRepeating("TriggerDamage", 1f, 1f);
+        InvokeRepeating("TriggerThirst", 2f, 2f);
     }
 
     void Update()
@@ -30,11 +36,23 @@ public class Plant : MonoBehaviour
         TakeDamage(1);
     }
 
+    void TriggerThirst()
+    {
+        GetThirsty(1);
+    }
+
     void TakeDamage(int damage)
     {
         currentHealth -= damage;
 
         healthbar.SetHealth(currentHealth);
+    }
+
+    void GetThirsty(int thirst)
+    {
+        currentThirst -= thirst;
+
+        thirstbar.SetThirst(currentThirst);
     }
 
     public void GainHealth(int health)
