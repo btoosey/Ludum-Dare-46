ad65e7c4ec1e2ebdc1ffc5d46420d0e4d0222a19
Ben Toosey <41492238+btoosey@users.noreply.github.com>
2020-04-20 16:50:30 +0100

    FEAT: Finalised game and build settings

---
+++ Ludum Dare 46/Assets/Scripts/Counter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    public Text flyCounter;
+
+    void Update()
+    {
+        flyCounter.text = GameObject.FindGameObjectsWithTag("Fly").Length.ToString();
+    }
+}
